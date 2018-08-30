44dbf7838723015b710d221c156decd312350538
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-08-30 19:03:48 +0100

    Activity list route

--- app.js
+++ app.js
@@ -6,17 +6,30 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(function(req, res, next){
+
+app.get('/', function (req, res) {
    // respond with json
   if (/(ld\+)?json/.test(req.headers.accept)) {
     request.get({ url: "https://www.openactive.io/ns/oa.jsonld" }, function(error, response, body) { 
       if (!error && response.statusCode == 200) { 
-          res.json(JSON.parse(body));
+        res.json(JSON.parse(body));
       } 
     });
   } else {
     res.redirect('https://www.openactive.io/');
-    return;
+  }
+});
+
+app.get('/activity-list', function (req, res) {
+   // respond with json
+  if (/(ld\+)?json/.test(req.headers.accept)) {
+    request.get({ url: "https://www.openactive.io/activity-list/activity-list.jsonld" }, function(error, response, body) { 
+      if (!error && response.statusCode == 200) { 
+        res.json(JSON.parse(body));
+      } 
+    });
+  } else {
+    res.redirect('https://www.openactive.io/activity-list/');
   }
 });
 
