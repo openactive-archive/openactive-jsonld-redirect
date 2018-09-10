83363b413be8b02c39bab496646322d84596b59c
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-09-10 19:34:39 +0100

    Added beta namespace

--- app.js
+++ app.js
@@ -33,4 +33,17 @@ app.get('/activity-list', function (req, res) {
   }
 });
 
+app.get('/ns-beta', function (req, res) {
+   // respond with json
+  if (/(ld\+)?json/.test(req.headers.accept)) {
+    request.get({ url: "https://www.openactive.io/ns-beta/beta.jsonld" }, function(error, response, body) { 
+      if (!error && response.statusCode == 200) { 
+        res.json(JSON.parse(body));
+      } 
+    });
+  } else {
+    res.redirect('https://www.openactive.io/ns-beta/');
+  }
+});
+
 module.exports = app;
